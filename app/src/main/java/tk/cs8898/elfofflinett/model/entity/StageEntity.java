658951477792b52b71f19f274c57e306ff1d0071
Christian Schmied <cs8898@gmx.de>
2018-07-27 18:02:48 +0200

    fully integrated fm4festival

--- app/src/main/java/tk/cs8898/elfofflinett/model/entity/StageEntity.java
+++ app/src/main/java/tk/cs8898/elfofflinett/model/entity/StageEntity.java
@@ -4,11 +4,15 @@ import java.util.List;
 
 public class StageEntity {
     private String name;
+    private String colorA;
+    private String colorB;
     private List<ActEntity> acts;
 
-    public StageEntity(String name, List<ActEntity> acts) {
+    public StageEntity(String name,String colorA, String colorB, List<ActEntity> acts) {
         this.name = name;
         this.acts = acts;
+        this.colorA = colorA;
+        this.colorB = colorB;
     }
 
     public String getName() {
@@ -17,6 +21,22 @@ public class StageEntity {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getColorA() {
+        return colorA;
+    }
+
+    public void setColorA(String colorA) {
+        this.colorA = colorA;
+    }
+
+    public String getColorB() {
+        return colorB;
+    }
+
+    public void setColorB(String colorB) {
+        this.colorB = colorB;
     }
 
     public List<ActEntity> getActs() {
